bdfd09d7b15f4e1fa11863ac62f82b61612c979b
LeondavidZipp <leondavidzipp@gmx.de>
2024-04-29 12:19:08 +0200

    added & fixed user api tests

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -53,7 +53,7 @@ class PublicUserApiTests(TestCase):
             'password': 'testpass123',
             'name': 'Test Name'
         }
-        create_user(payload)
+        create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
@@ -67,7 +67,7 @@ class PublicUserApiTests(TestCase):
             'password': 'test',
             'name': 'Test Name'
         }
-        res = self.client.get(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
