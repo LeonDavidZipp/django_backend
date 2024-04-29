9ba66798dc27cc818b8ab9d973c29a382cf83713
Leon David Zipp <leondavidzipp@gmx.de>
2024-04-29 22:48:09 +0200

    no noteworthy changes

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -44,7 +44,7 @@ class PublicUserApiTests(TestCase):
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
 
-    def test_user_with_email_exists(self):
+    def test_user_with_email_exists_error(self):
         """
         Test if error returned when user with this specific email already exists
         """
@@ -53,7 +53,7 @@ class PublicUserApiTests(TestCase):
             'password': 'testpass123',
             'name': 'Test Name'
         }
-        create_user(payload)
+        create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
@@ -64,10 +64,10 @@ class PublicUserApiTests(TestCase):
         """
         payload = {
             'email': 'test@example.com',
-            'password': 'test',
+            'password': 'pw',
             'name': 'Test Name'
         }
-        res = self.client.get(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
